c773afcfa623ab54996b15f817ac1118a7f9fef9
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-03-07 15:29:13 +0100

    feat: :chart_with_upwards_trend: Add GTAG Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
@@ -18,6 +19,22 @@ const GlobalStyle = createGlobalStyle`
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
+            {/* GOOGLE ANALYTICS */}
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-ZE9LYXPGV9"
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){window.dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'GA_MEASUREMENT_ID');
+                `}
+            </Script>
+            {/* GOOGLE ANALYTICS END*/}
+
             <GlobalStyle />
             <Component {...pageProps} />
         </>
